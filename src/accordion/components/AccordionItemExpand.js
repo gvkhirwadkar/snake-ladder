507ef750f0559eab42eebc44ad39472bf11b8a1f
gvkhirwadkar <gvkhirwadkar@gmail.com>
2023-12-10 19:39:44 +0530

    Expand animated

--- src/accordion/components/AccordionItemExpand.js
+++ src/accordion/components/AccordionItemExpand.js
@@ -2,9 +2,13 @@ import React from "react";
 import styles from "./AccordionItemExpand.module.css";
 
 export default function AccordionItemExpand({ isExpanded, content }) {
-  return (
-    isExpanded && (
-      <section className={styles.expandedSection}>{content}</section>
-    )
-  );
+  let sectionClass = "";
+
+  if (isExpanded) {
+    sectionClass = `${styles.section} ${styles.expandedSection}`;
+  } else {
+    sectionClass = `${styles.section} ${styles.collapsedSection}`;
+  }
+
+  return <section className={sectionClass}>{content}</section>;
 }
--- src/accordion/components/AccordionItemHeader.js
+++ src/accordion/components/AccordionItemHeader.js
@@ -1,6 +1,5 @@
 import React from "react";
-import styles from "./AccordionItemHeader.module.css";
 
 export default function AccordionItemHeader({ label = "" }) {
-  return <span className={styles.headerText}>{label}</span>;
+  return <span>{label}</span>;
 }
